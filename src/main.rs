9165d774c1c218f03837b7b79000aca0bcf8bcbe
May <MejeC10@gmail.com>
2023-04-03 17:23:04 -0400

    Add hello world printing

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,16 @@ use core::panic::PanicInfo;
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    let hello = b"Hello, World!"; // The prefix b makes it a straight binary array
+    let vga_buffer = 0xb8000 as *mut u8; // 753664 in binary. We cast it to *mut u8, a mutable pointer of bytes
+    let mut i = 0;
+    for byte in hello.iter() { // iter allows us to loop over the bytes
+        unsafe {
+            *vga_buffer.add(i as usize * 2) = *byte; // Dereference the borrowed byte and buffer, make one part the character
+            *vga_buffer.add(i as usize * 2 + 1) = 0xb // preset color
+        }
+        i += 1; // incremeent i
+    }
     rust_os::hlt_loop();
 }
 
